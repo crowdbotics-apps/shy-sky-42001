8062bf71b32a0f267b0bc0a0a74c55dddfc88ba4
Crowdbotics <team@crowdbotics.com>
2023-07-08 13:57:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shy-sky-42001",
+  url: "https://shy-sky-42001.botics.co",
+  api: "https://shy-sky-42001.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
